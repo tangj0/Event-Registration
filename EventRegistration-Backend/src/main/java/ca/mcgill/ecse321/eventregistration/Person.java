36426a1457a0602add4f53597ce9dc230c32d108
tangj0 <jtang186@gmail.com>
2020-02-16 22:19:34 -0500

    gradle wrapper update

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/Person.java
@@ -0,0 +1,27 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Person{
+   private String name;
+
+public void setName(String value) {
+    this.name = value;
+}
+@Id
+public String getName() {
+    return this.name;
+}
+   private RegistrationManager registrationManager;
+   
+   @ManyToOne(optional=false)
+   public RegistrationManager getRegistrationManager() {
+      return this.registrationManager;
+   }
+   
+   public void setRegistrationManager(RegistrationManager registrationManager) {
+      this.registrationManager = registrationManager;
+   }
+   
+   }
---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/Registration.java
@@ -0,0 +1,51 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Set;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Registration{
+   private Integer id;
+
+public void setId(Integer value) {
+    this.id = value;
+}
+@Id
+public Integer getId() {
+    return this.id;
+}
+   private Event event;
+   
+   @ManyToOne(optional=false)
+   public Event getEvent() {
+      return this.event;
+   }
+   
+   public void setEvent(Event event) {
+      this.event = event;
+   }
+   
+   private RegistrationManager registrationManager;
+   
+   @ManyToOne(optional=false)
+   public RegistrationManager getRegistrationManager() {
+      return this.registrationManager;
+   }
+   
+   public void setRegistrationManager(RegistrationManager registrationManager) {
+      this.registrationManager = registrationManager;
+   }
+   
+   private Set<Person> particiant;
+   
+   @ManyToMany
+   public Set<Person> getParticiant() {
+      return this.particiant;
+   }
+   
+   public void setParticiant(Set<Person> particiants) {
+      this.particiant = particiants;
+   }
+   
+   }
